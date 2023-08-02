fe9a31c019084737d5669cf0488cbfa1a0c03bc2
pradyumna1198 <92784304+pradyumna1198@users.noreply.github.com>
2023-08-02 14:14:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,5 +4,23 @@ function volume_sphere() {
   return volume;
   
 } 
+const radiusInput = document.getElementById('radius');
+const calculateButton = document.getElementById('submit');
+const volumeField = document.getElementById('volume');
+
+calculateButton.addEventListener('click', calculateVolume);
+
+function calculateVolume() {
+  const radius = parseFloat(radiusInput.value);
+  
+  if (isNaN(radius)) {
+    volumeField.textContent = 'Invalid input';
+    return;
+  }
+
+  const volume = volume_sphere(radius);
+  volumeField.textContent = `Volume: ${volume.toFixed(2)}`;
+}
+
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
